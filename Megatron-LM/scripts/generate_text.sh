05f03486755133d1e249f16621e6ce4bf1b2882d
RAHUL Danu <56379013+rahul-art@users.noreply.github.com>
2021-04-24 20:00:11 +0530

    Update generate_text.sh

--- Megatron-LM/scripts/generate_text.sh
+++ Megatron-LM/scripts/generate_text.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CHECKPOINT_PATH=/kaggle/working/DeepSpeedExamples/Megatron-LM/gpt2
+CHECKPOINT_PATH=/kaggle/working/DeepSpeedExamples/Megatron-LM/megatron-gpt2-345m
 MPSIZE=1
 NLAYERS=24
 NHIDDEN=1024
